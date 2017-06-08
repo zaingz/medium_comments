e4a7591927d3c661aa000279a0cefc5e2d9b897b
zainzafar-q <zain.zafar@qubit.com>
2017-06-08 13:16:37 +0500

    pack issue fix finally

--- app/javascript/packs/components/hello/index.js
+++ app/javascript/packs/components/hello/index.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import axios from 'axios'
+// import React from 'react';
+//
+// export default class Hello extends React.Component {
+//   render() {
+//     axios.get('http://demo0532507.mockable.io/')
+//       .then(res => console.log(res.data))
+//
+//       axios.get('/pages/api')
+//         .then(res => console.log(res.data))
+//
+//     return <div>Hello {this.props.name}!</div>
+//   }
+// }
+//
 
 
-export default class Hello extends React.Component {
-  render() {
-    axios.get('http://demo0532507.mockable.io/')
-      .then(res => console.log(res.data))
-
-      axios.get('/pages/api')
-        .then(res => console.log(res.data))
-
-    return <div>Hello {this.props.name}!</div>
-  }
-}
-
-
+console.log('last attempt');
 // Render component with data
