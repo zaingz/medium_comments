a795852893ded48ed84910845157db9826c144c4
zainzafar-q <zain.zafar@qubit.com>
2017-06-09 17:51:35 +0500

    cookie added

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,89 +1,76 @@
+document.addEventListener("turbolinks:load", function(event) {
+    $('.select_toolbar').slideUp();
+    $('.post').mouseup(function() {
+        var text = getSelectedText();
+        var parse = $('.post-content').html().replace(/(<span[^>]*>)|(<\/span>)/ig, "");
 
+        $('.post-content').html(parse.replace(text, '<span class="highlight">' + text + '</span>'));
 
-
-
-    document.addEventListener("turbolinks:load", function(event) {
-  $('.select_toolbar').slideUp();
-      $('.post').mouseup(function() {
-          var text = getSelectedText();
-          var parse = $('.post-content').html().replace(/(<span[^>]*>)|(<\/span>)/ig, "");
-
-          $('.post-content').html(parse.replace(text, '<span class="highlight">'+text+'</span>'));
-
-          if (/(<span[^>]*>)|(<\/span>)/.test($('.post-content').html()) && text.length){
+        if (/(<span[^>]*>)|(<\/span>)/.test($('.post-content').html()) && text.length) {
             $.event.trigger({
-        			type: "textSelected",
-        			text: text,
-        			time: new Date()
-        		});
-          }else {
-            $.event.trigger({
-              type: "textSelectionRemoved",
-              text: text,
-              time: new Date()
+                type: "textSelected",
+                text: text,
+                time: new Date()
             });
-          }
+        } else {
+            $.event.trigger({
+                type: "textSelectionRemoved",
+                text: text,
+                time: new Date()
+            });
+        }
+
+    });
 
 
 
 
-
-      });
-
-
-
-
-      $(document).on("textSelectionRemoved", function(event) {
+    $(document).on("textSelectionRemoved", function(event) {
         console.log('you babes not');
 
-      $('.select_toolbar').slideUp(300);
+        $('.select_toolbar').slideUp(300);
 
-      });
-
-
+    });
 
 
 
-      $(document).on("textSelected", function(event) {
+    $(document).on("textSelected", function(event) {
         console.log('you babes');
 
         $('.actions').show();
         $('.input').hide();
         $('.select_toolbar').slideDown(300);
-      });
+    });
 
 
 
+    function getSelectedText() {
+        if (window.getSelection) {
+            return window.getSelection().toString();
+        } else if (document.selection) {
+            return document.selection.createRange().text;
+        }
+        return '';
+    }
 
-      function getSelectedText() {
-          if (window.getSelection) {
-              return window.getSelection().toString();
-          } else if (document.selection) {
-              return document.selection.createRange().text;
-          }
-          return '';
-      }
-
-      $('.note').click(function() {
+    $('.note').click(function() {
         $('.actions').hide(100);
         $('.input').show(100);
 
         window.action = 'note';
-      });
+    });
 
 
-        $('.comment').click(function() {
-          $('.actions').hide(100);
-          $('.input').show(100);
+    $('.comment').click(function() {
+        $('.actions').hide(100);
+        $('.input').show(100);
 
-          window.action = 'comment';
-        });
-
-
+        window.action = 'comment';
+    });
 
 
 
-      $("#submit_btn").click(function(e){
+    $("#submit_btn").click(function(e) {
         var val = $('#input-box').val();
         $('#input-box').val('');
 
@@ -92,28 +79,44 @@
         var select_text = $('.highlight').text();
         var current_p_content = $('.highlight').parent().closest('p');
 
-         var current_p = current_p_content.html().replace(/(<span[^>]*>)|(<\/span>)/ig, "");
-         current_p = current_p.replace(select_text, '<b class="h-note">'+select_text+'</b>');
+        var current_p = current_p_content.html().replace(/(<span[^>]*>)|(<\/span>)/ig, "");
+        current_p = current_p.replace(select_text, '<b class="h-note">' + select_text + '</b>');
 
 
-         var position = current_p_content.position();
-         var style = 'style="top:' + position.top + 'px;';
+        var position = current_p_content.position();
+        var style = 'style="top:' + position.top + 'px;';
 
-         var heading;
+        var heading;
         if (window.action === 'note') {
-           heading = 'Private Note';
+            heading = 'Private Note';
+        } else {
+            heading = 'Comment';
         }
-        else{
-           heading = 'Comment';
-        }
 
-        $('.highlight').parent().closest('p').html('<p>'+ current_p +'<div class="h-comment" ' +   style + '">'  +   heading + '<div class="value">' +    val + '</div>' +'</div> </p>');
+        $('.highlight').parent().closest('p').html('<p>' + current_p + '<div class="h-comment" ' + style + '">' + heading + '<div class="value">' + val + '</div>' + '</div> </p>');
 
 
 
 
 
 
-      });
+
+
+
+        $.ajax({
+          url: 'posts/update',
+          type: 'POST',
+          data: {content: $('.post-content').html()},
+
+          dataType: 'json',
+          success: function(data) {
+            console.log('sucess');
+          }
+          }).done(function(msg) {
+              console.log('done');
+          });
+
 
     });
+
+});
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,17 @@
 class PostsController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: :update
   def index
-    @post = Post.first
+    if cookies[:post]
+      @post = YAML::load cookies[:post]
+    else
+      @post = Post.first
+      cookies[:post] = YAML::dump @post
+    end
+  end
+
+
+  def update
+    Post.first.update(content: params[:content])
+    cookies[:post] = YAML::dump Post.first
   end
 end
