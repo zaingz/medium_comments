641f03726d2828802e99d7a3b5851a31426f4b69
Zain Zafar <zain.zafar@tessian-000331.mynet>
2019-01-17 22:13:00 +0000

    almost ready-just remove comments

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,16 +1,16 @@
+var data
+var action
 document.addEventListener("turbolinks:load", function(event) {
     $('.select_toolbar').slideUp();
 
-    if (localStorage.getItem('content')){
-      $('.post-content').html(localStorage.getItem('content'))
-    }
-    $('.post').mouseup(function() {
+    $( '*' ).add( ".post" ).on('mouseup', function() {
+    //$( ".post" ).on('mouseup', function() {
         var text = getSelectedText();
-        var parse = $('.post-content').html().replace(/(<span[^>]*>)|(<\/span>)/ig, "");
+        var parse = $('.post-content').html().replace(/(<span>)|(<\/span>)/, "");
 
         $('.post-content').html(parse.replace(text, '<span class="highlight">' + text + '</span>'));
 
-        if (/(<span[^>]*>)|(<\/span>)/.test($('.post-content').html()) && text.length) {
+        if (/(<span>)|(<\/span>)/.test($('.post-content').html()) && text.length) {
             $.event.trigger({
                 type: "textSelected",
                 text: text,
@@ -23,30 +23,18 @@ document.addEventListener("turbolinks:load", function(event) {
                 time: new Date()
             });
         }
-
     });
-
-
 
 
     $(document).on("textSelectionRemoved", function(event) {
-        console.log('you babes not');
-
         $('.select_toolbar').slideUp(300);
-
     });
 
-
-
     $(document).on("textSelected", function(event) {
-        console.log('you babes');
-
         $('.actions').show();
         $('.input').hide();
         $('.select_toolbar').slideDown(300);
     });
-
-
 
     function getSelectedText() {
         if (window.getSelection) {
@@ -61,7 +49,7 @@ document.addEventListener("turbolinks:load", function(event) {
         $('.actions').hide(100);
         $('.input').show(100);
 
-        window.action = 'note';
+        action = 'note';
     });
 
 
@@ -69,58 +57,47 @@ document.addEventListener("turbolinks:load", function(event) {
         $('.actions').hide(100);
         $('.input').show(100);
 
-        window.action = 'comment';
+        action = 'comment';
     });
-
 
 
     $("#submit_btn").click(function(e) {
         var val = $('#input-box').val();
         $('#input-box').val('');
 
-
-
         var select_text = $('.highlight').text();
         var current_p_content = $('.highlight').parent().closest('p');
 
-        var current_p = current_p_content.html().replace(/(<span[^>]*>)|(<\/span>)/ig, "");
+        var current_p = current_p_content.html().replace(/(<span>)|(<\/span>)/ig, "");
         current_p = current_p.replace(select_text, '<b class="h-note">' + select_text + '</b>');
-
 
         var position = current_p_content.position();
         var style = 'style="top:' + position.top + 'px;';
 
         var heading;
-        if (window.action === 'note') {
+        if (action == 'note') {
             heading = 'Private Note';
         } else {
             heading = 'Comment';
         }
 
-        $('.highlight').parent().closest('p').html('<p>' + current_p + '<div class="h-comment" ' + style + '">' + heading + '<div class="value">' + val + '</div>' + '</div> </p>');
+ 
+        if (Object.prototype.toString.call(data) !== '[object Array]'){
+            data = new Array
+        }
 
-
-
-
-
-        localStorage.setItem('content', $('.post-content').html());
-
-
-        //
-        // $.ajax({
-        //   url: 'posts/update',
-        //   type: 'POST',
-        //   data: {content: $('.post-content').html()},
-        //
-        //   dataType: 'json',
-        //   success: function(data) {
-        //     console.log('sucess');
-        //   }
-        //   }).done(function(msg) {
-        //       console.log('done');
-        //   });
-
+        Array.prototype.push.apply(data, [{text: current_p, comment: val, type: heading, style: style}])
+ 
+        (function(data){
+            data.reverse().forEach(function(element){
+                var text = element.text;
+                var comment = element.comment;
+                var type = element.type;
+                var style = element.style;
+                $('.highlight').parent().html('<p>' + text + '<div class="h-comment" ' + style + '">' + type + '<div class="value">' + comment + '</div>' + '</div> </p>');
+    
+            })
+        })(data)
 
     });
-
 });
